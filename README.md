5b584a3286e392dac2b855432404d098b0d0f6ef
Sebastian Willowhawk <bazindigo@gmail.com>
2021-02-16 01:40:45 -0700

    updated readme, added GRUB support, added some more firmware modules

--- README.md
+++ README.md
@@ -1,12 +1,16 @@
 # Installing Arch Linux on a Macbook Pro 2019
-* requirements:
-  * a partition set up for your linux installation
-  * a separate partition for to use as your mac's second EFI partition
+## Requirements
+* Macbook Pro 2019 16"
+* one partition for your linux installation
+  * already partitioned from macOS or using a disk utility in the arch iso
+* one partition for your new EFI (different than the EFI partition already on your mac)
+  * again, already partitioned from macOS or using a disk utility in arch iso
 
 ## Steps
-1. `pacman -Syy && pacman -S git`
-2. `git clone https://github.com/Bazindigo/Arch-Install-Mac.git`
-3. edit script variables:
+1. Boot into arch iso from a USB
+2. `pacman -Syy && pacman -S git`
+3. `git clone https://github.com/Bazindigo/Arch-Install-Mac.git`
+4. edit script variables:
     * `ROOT_PASSWORD`: the password for root in your installation
     * `USERNAME`: the username of the user to configure
     * `USR_PASSWORD`: the password of the new user
@@ -17,4 +21,6 @@
     * `ESP_DEVICE`: the partition to use as the esp (same format as PARTITION_DEVICE)
     * `REGION`: the region you are in
     * `CITY`: the city you are in within the region
-4. `./install_arch.sh`
+5. If you want to install with systemd: `./install_arch.sh systemd`. 
+    * If you want to install with GRUB: `./install_arch.sh grub`
+6. After any finishing touches on your install, `reboot` and hold option on startup. Select the new option in the menu.
--- install_arch.sh
+++ install_arch.sh
@@ -8,57 +8,112 @@ declare ESP_NAME = "Arch Boot"
 declare ESP_DEVICE = "/dev/nvme0n1p3"
 declare REGION = "America"
 declare CITY = "Denver"
+declare BOOTMANAGER = $1
 
+# set bootmanager flag
+if [$BOOTMANAGER == "systemd"]
+then
+    declare BOOTMANAGER = 0
+elif [$BOOTMANAGER == "grub"]
+then
+    declare BOOTMANAGER = 1
+else
+then
+    echo "Usage: install_arch [systemd | grub]"
+    exit 1
+fi
+
+# format the main partition and mount it
 mkfs.ext4 -L $PARTITION_NAME /dev/partition_label
 mkdir -p /mnt/boot
 mount $PARTITION_DEVICE /mnt
-pacman -S wget
+
+# set up pacman --noconfirm keys
+pacman -Syy
+pacman --noconfirm -S wget
 wget http://dl.t.t2linux.org/archlinux/key.asc
 pacman-key --add key.asc
 pacman-key --finger 7F9B8FC29F78B339
 pacman-key --lsign-key 7F9B8FC29F78B339
 rm key.asc
-pacstrap /mnt linux-mbp linux-mbp-headers linux-firmware base base-devel grub-efi-x86_64 zsh zsh-completions vim nano strace git efibootmgr dialog wpa_supplicant
 
+# pacstrap
+pacstrap /mnt linux-mbp linux-mbp-headers linux-firmware base base-devel grub-efi-x86_64 zsh zsh-completions vim nano strace git efibootmgr dialog wpa_supplicant man-db
+
+# chroot, set up root, user account, hfsprogs, esp
 arch-chroot /mnt /bin/bash << "EOT"
 
 echo $ROOT_PASSWORD | passwd --stdin
 useradd -m -g users -G wheel,storage,power -s /bin/zsh $USERNAME
 echo $USR_PASSWORD | passwd MYUSERNAME --stdin
 echo "${USERNAME} ALL = (ALL) NOPASSWD: ALL" >> /etc/sudoers
-su $USERNAME -c git clone https://aur.archlinux.org/hfsprogs.git && cd hfsplus && makepkg -si && mkfs.hfsplus -v $ESP_NAME $ESP_DEVICE && exit
+su $USERNAME -c git clone https://aur.archlinux.org/hfsprogs.git && cd hfsplus && makepkg -si
+mkfs.hfsplus -v $ESP_NAME $ESP_DEVICE
+su $USERNAME -c git clone https://aur.archlinux.org/aic94xx-firmware.git && cd aic94xx-firmware && makepkg -si
+su $USERNAME -c git clone https://aur.archlinux.org/wd719x-firmware.git && cd wd719x-firmware && makepkg -si
+su $USERNAME -c git clone https://aur.archlinux.org/upd72020x-fw.git && cd upd72020x-fw && makepkg -si
 
 EOT
 
+# mount esp and genfstab
 mount /dev/nvme0n1p3 /mnt/boot
 genfstab -pU /mnt >> /mnt/etc/fstab
 
+# chroot for basic config
 arch-chroot /mnt /bin/bash << "EOT"
 
+# set time
 ln -sf /usr/share/zoneinfo/$REGION/$CITY /etc/localtime
 hwclock --systohc
 
+# set hostname/network stuff
 echo $HOST_NAME >> /etc/hostname
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1 localhost" >> /etc/hosts
 echo "127.0.1.1 ${HOST_NAME}" >> /etc/hosts
 
+# set locale
 echo LANG=en_US.UTF-8 >> /etc/locale.conf
 echo LANGUAGE=en_US >> /etc/locale.conf
 echo LC_ALL=C >> /etc/locale.conf
 sed -i 's/#en_US.UTF-8/en_US.UTF-8/' /etc/locale.gen
 locale-gen
 
-pacman -S linux-mbp linux-mbp-headers linux-firmware intel-ucode
+# pacman again
+pacman --noconfirm -S linux-mbp linux-mbp-headers linux-firmware intel-ucode
 
-declare -x SYSTEMD_RELAX_ESP_CHECKS=1
-declare -x SYSTEMD_RELAX_XBOOTLDR_CHECKS=1
-bootctl --path=/boot --no-variables install
-systemctl mask systemd-boot-system-token.service
+# set up chosen bootloader on esp
+if [$BOOTMANAGER == 0]
+then
+    declare -x SYSTEMD_RELAX_ESP_CHECKS=1
+    declare -x SYSTEMD_RELAX_XBOOTLDR_CHECKS=1
+    bootctl --path=/boot --no-variables install
+    systemctl mask systemd-boot-system-token.service
+elif [$BOOTMANAGER == 1]
+then
+    touch /boot/mach_kernel
+    mkdir -p /boot/EFI/BOOT && touch /boot/EFI/BOOT/mach_kernel
+    grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB --removable
+    grub-mkconfig -o /boot/grub/grub.cfg
+    mv /boot/efi/BOOT/System /boot/
+    rm -r /boot/efi
+    sed -i 's/<plist version="1.0">/<?xml version="1.0" encoding="UTF-8"?>\n<plist version="1.0">' /boot/System/Library/CoreServices/SystemVersion.plist
+    sed -i 's/grub/Linux/' /boot/System/Library/CoreServices/SystemVersion.plist
+    sed -i 's/2.04/Arch Linux/' /boot/System/Library/CoreServices/SystemVersion.plist
+fi
 
+# get nice icon for boot menu
+pacstrap -S librsvg libicns
+wget -O /tmp/archlinux.svg https://archlinux.org/logos/archlinux-icon-crystal-64.svg
+rsvg-convert -w 128 -h 128 -o /tmp/archlogo.png /tmp/archlinux.svg
+png2icns /boot/.VolumeIcon.icns /tmp/archlogo.png
+rm /tmp/archlogo.png /tmp/archlinux.svg
+
+# all done, exit everything now
 sync
 
 EOT
 
 sync
 echo "reboot when ready..."
+exit 0
